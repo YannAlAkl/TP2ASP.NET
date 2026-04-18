6db2e6f7fc9a61679ec26435e54541b58fab0117
YannAlAkl <alaklyann56@gmail.com>
2026-04-18 13:59:50 -0400

    Nouveau commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,102 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Yann_Al_Akl_WS1_TP2_Développement_Web_Serveur__1.Models.ViewModels;
+
+
+namespace Yann_Al_Akl_WS1_TP2_Développement_Web_Serveur__1.Controllers
+{
+	public class UserController : Controller
+	{
+		private readonly UserManager<IdentityUser> _userManager;
+
+		public UserController(UserManager<IdentityUser> userManager)
+		{
+			_userManager = userManager;
+		}
+
+		// GET: UserController
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+		// GET: UserController/Details/5
+		public ActionResult Details(int id)
+		{
+			return View();
+		}
+
+		// GET: UserController/Create
+		public async Task<ActionResult> Create()
+		{
+			return View();
+		}
+
+		// POST: UserController/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<ActionResult>	 Create(createUserViewModel vm)
+		{
+			var user = new IdentityUser { UserName = vm.Username, Email = vm.Email };
+				var result = await _userManager.CreateAsync(user, vm.Password);
+				if (result.Succeeded)
+				{
+					return RedirectToAction(nameof(Index));
+				}
+				else
+				{
+					foreach (var error in result.Errors)
+					{
+						ModelState.AddModelError(string.Empty, error.Description);
+					}
+					return View(vm);
+			}
+
+			
+			
+		}
+
+		// GET: UserController/Edit/5
+		public ActionResult Edit(int id)
+		{
+			return View();
+		}
+
+		// POST: UserController/Edit/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+
+		// GET: UserController/Delete/5
+		public ActionResult Delete(int id)
+		{
+			return View();
+		}
+
+		// POST: UserController/Delete/5
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Delete(int id, IFormCollection collection)
+		{
+			try
+			{
+				return RedirectToAction(nameof(Index));
+			}
+			catch
+			{
+				return View();
+			}
+		}
+	}
+}
---
+++ Models/ViewModels/createUserViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Yann_Al_Akl_WS1_TP2_Développement_Web_Serveur__1.Models.ViewModels
+{
+	public class createUserViewModel
+	{
+		public string Username { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public string ConfirmPassword { get; set; }
+	}
+}
