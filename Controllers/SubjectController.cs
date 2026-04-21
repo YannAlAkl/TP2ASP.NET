c04bed5d02f6560e89fdec79636caf6f6eeae6a3
YannAlAkl <alaklyann56@gmail.com>
2026-04-21 13:48:07 -0400

    Commit 2

--- Controllers/SubjectController.cs
+++ Controllers/SubjectController.cs
@@ -11,166 +11,170 @@ using Yann_Al_Akl_WS1_TP2_Développement_Web_Serveur__1.Models;
 
 namespace Yann_Al_Akl_WS1_TP2_Développement_Web_Serveur__1.Controllers
 {
-    public class SubjectController : Controller
-    {
-        private readonly ApplicationDbContext _context;
+	public class SubjectController : Controller
+	{
+		private readonly ApplicationDbContext _context;
 
-        public SubjectController(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+		public SubjectController(ApplicationDbContext context)
+		{
+			_context = context;
+		}
 
-        // GET: Subjects
-        public async Task<IActionResult> Index()
-        {
-            var applicationDbContext = _context.Subjects.Include(s => s.Category).Include(s => s.User);
-            return View(await applicationDbContext.ToListAsync());
-        }
+		// GET: Subjects
+		public async Task<IActionResult> Index()
+		{
+			var applicationDbContext = _context.Subjects.Include(s => s.Category).Include(s => s.User);
+			return View(await applicationDbContext.ToListAsync());
+		}
 
 		// GET: Subjects/Details/5
 		public async Task<IActionResult> Details(int? id)
 		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
 			var subject = await _context.Subjects
+				.Include(s => s.Category)
+				.Include(s => s.User)
 				.Include(s => s.Messages)
 				.FirstOrDefaultAsync(m => m.Id == id);
+			if (subject == null)
+			{
+				return NotFound();
+			}
 
+			RedirectToAction("Details", "Subject", new { id = subject.Id });
+
+			return View(subject);
+		}
+
+		// GET: Subjects/Create
+		public IActionResult Create()
+		{
+			ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
+			ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
+			return View();
+		}
+
+		// POST: Subjects/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[Authorize]
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Title,Content,CreatedAt,ViewCount,IsDeleted,CategoryId,UserId")] Subject subject)
+		{
+			if (ModelState.IsValid)
+			{
+				_context.Add(subject);
+				await _context.SaveChangesAsync();
+				return RedirectToAction("Details", "Subject", new { id = subject.Id });
+			}
+			ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", subject.CategoryId);
+			ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", subject.UserId);
+			return View(subject);
+		}
+
+		// GET: Subjects/Edit/5
+		[Authorize]
+		public async Task<IActionResult> Edit(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var subject = await _context.Subjects.FindAsync(id);
+			if (subject == null)
+			{
+				return NotFound();
+			}
+			ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", subject.CategoryId);
+			ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", subject.UserId);
+			return View(subject);
+		}
+
+		// POST: Subjects/Edit/5
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[Authorize]
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content,CreatedAt,ViewCount,IsDeleted,CategoryId,UserId")] Subject subject)
+		{
+			if (id != subject.Id)
+			{
+				return NotFound();
+			}
+
+			if (ModelState.IsValid)
+			{
+				try
+				{
+					_context.Update(subject);
+					await _context.SaveChangesAsync();
+				}
+				catch (DbUpdateConcurrencyException)
+				{
+					if (!SubjectExists(subject.Id))
+					{
+						return NotFound();
+					}
+					else
+					{
+						throw;
+					}
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", subject.CategoryId);
+			ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", subject.UserId);
+			return View(subject);
+		}
+
+		// GET: Subjects/Delete/5
+		[Authorize]
+		public async Task<IActionResult> Delete(int? id)
+		{
+			if (id == null)
+			{
+				return NotFound();
+			}
+
+			var subject = await _context.Subjects
+				.Include(s => s.Category)
+				.Include(s => s.User)
+				.FirstOrDefaultAsync(m => m.Id == id);
+			if (subject == null)
+			{
+				return NotFound();
+			}
+
+			return View(subject);
+		}
+
+		// POST: Subjects/Delete/5
+		[Authorize]
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var subject = await _context.Subjects.FindAsync(id);
 			if (subject != null)
 			{
-				foreach (var msg in subject.Messages)
-				{
-					// On force le chargement de l'utilisateur pour chaque message
-					msg.User = await _context.Users.FirstOrDefaultAsync(u => u.Id == msg.UserId);
-				}
+				subject.IsDeleted = true;
+				subject.Messages.All(m => m.IsDeleted = true);
 			}
-			return View(subject);
-		
-		}        
-        // GET: Subjects/Create
-		public IActionResult Create()
-        {
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name");
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id");
-            return View();
-        }
 
-        // POST: Subjects/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [Authorize]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Title,Content,CreatedAt,ViewCount,IsDeleted,CategoryId,UserId")] Subject subject)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(subject);
-                await _context.SaveChangesAsync();
-                return RedirectToAction("Details", "Subject", new { id = subject.Id });
-            }
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", subject.CategoryId);
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", subject.UserId);
-            return View(subject);
-        }
+			await _context.SaveChangesAsync();
+			return RedirectToAction(nameof(Index));
+		}
 
-        // GET: Subjects/Edit/5
-        [Authorize]
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var subject = await _context.Subjects.FindAsync(id);
-            if (subject == null)
-            {
-                return NotFound();
-            }
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", subject.CategoryId);
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", subject.UserId);
-            return View(subject);
-        }
-
-        // POST: Subjects/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [Authorize]
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Content,CreatedAt,ViewCount,IsDeleted,CategoryId,UserId")] Subject subject)
-        {
-            if (id != subject.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(subject);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!SubjectExists(subject.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CategoryId"] = new SelectList(_context.Categories, "Id", "Name", subject.CategoryId);
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", subject.UserId);
-            return View(subject);
-        }
-
-        // GET: Subjects/Delete/5
-        [Authorize]
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var subject = await _context.Subjects
-                .Include(s => s.Category)
-                .Include(s => s.User)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (subject == null)
-            {
-                return NotFound();
-            }
-
-            return View(subject);
-        }
-
-        // POST: Subjects/Delete/5
-        [Authorize]
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var subject = await _context.Subjects.FindAsync(id);
-            if (subject != null)
-            {
-                subject.IsDeleted = true;
-                subject.Messages.All(m => m.IsDeleted = true);
-            }
-
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool SubjectExists(int id)
-        {
-            return _context.Subjects.Any(e => e.Id == id);
-        }
-
-    }
+		private bool SubjectExists(int id)
+		{
+			return _context.Subjects.Any(e => e.Id == id);
+		}
+	}
 }
